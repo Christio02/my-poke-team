6e44e989dae875e12bcfdb9b4f01b4b53d2c609d
eirikekv <eirikekv@stud.ntnu.no>
2024-09-11 16:41:39 +0200

    Created Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1>Team-builder</h1>
+      <p>
+        Vivamus sit amet egestas magna. Aenean et lectus vitae massa interdum hendrerit sed vel turpis. Nulla consequat, augue ut condimentum vulputate.
+      </p>
+    </header>
+  );
+};
+
+export default Header;
