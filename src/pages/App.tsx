1c9b7031dd4975da4cf3d94df32606e82eef8c80
eirikekv <eirikekv@stud.ntnu.no>
2024-09-12 15:57:47 +0200

    header styling is finished and is responsive. Only routing remaining.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,12 @@
-import '../styles/App.css'
+import '../styles/App.css';
+import Header from '../components/Header.tsx';
 
 function App() {
-
   return (
     <>
-
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
