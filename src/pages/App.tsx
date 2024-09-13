137544db1f291ede6cfd624e7039985ab8f1ebd8
christgh <christopher.hoee@gmail.com>
2024-09-13 14:59:11 +0200

    feat: rebase main

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,14 @@
 import '../styles/app.css';
 import Nav from '../components/Nav.tsx';
 import Banner from '../components/Banner.tsx';
+import { TeamPage } from './TeamPage.tsx';
 
 function App() {
   return (
     <>
       <Nav />
       <Banner />
+      <TeamPage />
     </>
   );
 }
