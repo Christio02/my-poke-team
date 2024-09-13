19bfb22f3215c6a9a6a3c85aa95a02c06fe63fae
christgh <christopher.hoee@gmail.com>
2024-09-13 17:36:30 +0200

    fix: rebase 6

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,15 +1,7 @@
 import '../styles/team.css';
-import { FaRegStar } from 'react-icons/fa';
-import { FaStar } from 'react-icons/fa';
 import { useState } from 'react';
+import PokemonCard, { Pokemon } from './PokemonCard.tsx';
 // import PokemonCard from './PokemonCard.tsx';
-
-type Pokemon = {
-  name: string;
-  type: string;
-  hp: number;
-  isFavorite: boolean;
-};
 
 type FavoritesProps = {
   name: string;
@@ -20,42 +12,42 @@ export function Favorites({ name }: FavoritesProps) {
     {
       name: 'Bulbasaur',
       type: 'Grass',
-      hp: 200,
+      id: 1,
       isFavorite: true,
     },
     {
       name: 'Charmander',
       type: 'Fire',
-      hp: 250,
+      id: 2,
       isFavorite: false,
     },
     {
       name: 'Squirtle',
       type: 'Water',
-      hp: 220,
+      id: 3,
       isFavorite: true,
     },
     {
       name: 'Pidgey',
       type: 'Normal',
-      hp: 180,
+      id: 4,
       isFavorite: false,
     },
     {
       name: 'Jigglypuff',
       type: 'Fairy',
-      hp: 270,
+      id: 5,
       isFavorite: true,
     },
     {
       name: 'Psyduck',
       type: 'Water',
-      hp: 240,
+      id: 6,
       isFavorite: true,
     },
   ]);
 
-  const handleFavoriteClick = (index: number) => {
+  const toggleFavorite = (index: number) => {
     setPokemonData((prevData) =>
       prevData.map((pokemon, i) => (i === index ? { ...pokemon, isFavorite: !pokemon.isFavorite } : pokemon)),
     );
@@ -71,15 +63,22 @@ export function Favorites({ name }: FavoritesProps) {
         {pokemonData.map((pokemon, index) => (
           <div className="pokemon-card-container" key={index}>
             <ul>
-              <li>{pokemon.name}</li>
-              <li>{pokemon.type}</li>
-              <li>{pokemon.hp}</li>
+              <PokemonCard
+                name={pokemon.name}
+                type={pokemon.type}
+                id={pokemon.id}
+                isFavorite={pokemon.isFavorite}
+                onToggleFavorite={() => toggleFavorite(index)}
+              />
             </ul>
+<<<<<<< HEAD
             {/*<PokemonCard />*/}
             <button onClick={() => handleFavoriteClick(index)}>
               {!pokemon.isFavorite && <FaRegStar />}
               {pokemon.isFavorite && <FaStar />}
             </button>
+=======
+>>>>>>> c53ab86 (feat: add pokemonCard compoenent to favorite component)
           </div>
         ))}
       </section>
