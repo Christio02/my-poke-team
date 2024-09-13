9619a2d6db54e61b8da69de8b18f50d18e3e7870
christgh <christopher.hoee@gmail.com>
2024-09-13 14:49:09 +0200

    fix: rebase main

---
+++ src/pages/TeamPage.tsx
@@ -0,0 +1,14 @@
+import '../styles/teampage.css';
+// import { Banner } from '../components/Banner.tsx';
+import { Favorites } from '../components/Favorites.tsx';
+
+export function TeamPage() {
+  return (
+    <>
+      <main className="team-page">
+        {/*<Banner />*/}
+        <Favorites name="Chris" />
+      </main>
+    </>
+  );
+}
