c53ab86ff469aa95ffc9ccabca24cc2727b59583
christgh <christopher.hoee@gmail.com>
2024-09-13 17:29:10 +0200

    feat: add pokemonCard compoenent to favorite component

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,37 +1,46 @@
-
 import '../styles/pokemonCard.css';
-import Cradily from '../assets/cradilyPKMN.png';
+import Cradily from '../assets/cradilypng.png';
+import { FaRegStar, FaStar } from 'react-icons/fa';
 
+export type Pokemon = {
+  name: string;
+  type: string;
+  id: number;
+  isFavorite: boolean;
+  onToggleFavorite?: () => void;
+};
 
-
-
-
-export default function PokemonCard(){
-
-    let handleLeftclick;
-    let handleRightclick;
+export default function PokemonCard({ name, id, isFavorite, onToggleFavorite }: Pokemon) {
+  let handleLeftclick;
+  let handleRightclick;
 
   return (
     <section className="pokemonCard">
       <div className="pokemonName">
-        <h2 className="pokemonNameHeader">Name of Pokemon</h2>
+        <h2 className="pokemonNameHeader">{name}</h2>
       </div>
       <div className="icon">
         <img className="pokemonImage" src={Cradily} alt="" />
       </div>
       <div className="buttons">
-        <button className="leftButton" onClick={handleLeftclick}>prevNum</button>
-        <button className="rightButton" onClick={handleRightclick}>nextNum</button>
+        <button className="leftButton" onClick={handleLeftclick}>
+          prevNum
+        </button>
+        <button className="rightButton" onClick={handleRightclick}>
+          nextNum
+        </button>
       </div>
+      <button onClick={onToggleFavorite}>
+        {!isFavorite && <FaRegStar size={30} />}
+        {isFavorite && <FaStar size={30} color="yellow" />}
+      </button>
       <div className="pokemonNumber">
-        <h3>pokemonNumber</h3>
+        <h3>{id}</h3>
       </div>
       <div className="types">
-        <div className="leftType">
-        </div>
-        <div className="rightType">
-        </div>
+        <div className="leftType"></div>
+        <div className="rightType"></div>
       </div>
     </section>
-  )
+  );
 }
